87d1cf1329d98182233b6b99dee1df44238520d0
Tara Roys <tlroys@gmail.com>
2013-08-20 14:10:23 -0500

    Created new project called Treebook. It will be a facebook clone.  Added status updates.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '8c96ef00384a7f0c61bff349796cdd72f6745412af5815e3a185a1138c912b030e59b9316e71263c79dd442642fc5e3a5cd42c22b31c8f6e090493b9ea0d5f64'
